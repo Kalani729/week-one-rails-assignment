6cf936c300aaa448914fd983b7d25f9374c8faaa
Mark Albright <kalani729@gmail.com>
2017-08-14 13:19:18 -0600

    Passing rspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :guides
+  get 'static/quotes'
+
+  get 'static/home'
+
+  root to: 'static#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
